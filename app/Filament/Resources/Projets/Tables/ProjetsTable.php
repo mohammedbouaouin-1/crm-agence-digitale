acf9557eedbb728e09f29dd6451e8021e7281eb8
mohammedbouaouin-1 <mohammed.bouaouin@usmba.ac.ma>
2026-09-20 23:43:15 +0000

    fix: Import du modele Projet dans ProjetsTable pour resoudre l'erreur de typage

--- app/Filament/Resources/Projets/Tables/ProjetsTable.php
+++ app/Filament/Resources/Projets/Tables/ProjetsTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Projets\Tables;
 
+use App\Models\Projet;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
